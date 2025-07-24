9a480fb4a3d7956f5d7dc8c57ac94f70dab7cdfe
anwarsahil916-coder <anwarsahil916@gmail.com>
2025-07-24 16:33:45 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = input("Enter the first number : ")
+a = int(a)
+b = input("Enter the Second number : ")
+b = int(b)
+
+print("\nnAddition is : ",a+b)
+print("subtraction is : ",a-b)
+print("Multiplication is : ",a*b)
+print("Division is : ",a/b)
---
+++ full_name.py
@@ -0,0 +1,5 @@
+f_name = input("Enter your First name : ")
+l_name = input("Enter your Last name : ")
+
+full_name = f_name + " " + l_name
+print("Your Full Name is : ",full_name)
