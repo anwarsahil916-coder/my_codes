ff1fb5290062b07e42b96c826ff181d044674eb3
anwarsahil916-coder <anwarsahil916@gmail.com>
2025-07-25 22:24:12 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number : "))
+
+if (number % 2 == 0):
+    print("The number",number,"is Even.")
+else:
+    print("The number ",number," is Odd.") 
---
+++ sum.py
@@ -0,0 +1,6 @@
+number = list(range(1, 51))
+sum = 0
+for i in number:
+    sum = sum + i
+    
+print("The sum of numbers from 1 to 50 is : ", sum)
