6dd549f5f2e655056b366865c27bb6137539451c
anwarsahil916-coder <anwarsahil916@gmail.com>
2025-07-31 11:22:21 +0530

    Add files via upload

---
+++ get_first_five_element.py
@@ -0,0 +1,7 @@
+org_list = [1,2,3,4,5,6,7,8,9,10]
+# Extracts the first five elements from the list.
+print("Extracts the first five elements : ",org_list[:5])
+# Reversed extracts elements.
+rev_list = org_list[:5]
+rev_list.reverse()
+print("Reversed extracts elements : ",rev_list)
